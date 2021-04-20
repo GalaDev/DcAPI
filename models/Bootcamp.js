6ede0f73dd957851d13d4722fd69fdc63fb5aa05
Andy Gala <livelifebrands@gmail.com>
2021-04-19 20:27:48 -0700

    Add schemas

---
+++ models/Bootcamp.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    unique: true,
+    trim: true,
+    maxlength: [500, 'Description cannot be more than 500 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please add valid URL with HTTP or HTTPS',
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number cannot be longer than 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add valid email',
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
+    ],
+  },
+});
