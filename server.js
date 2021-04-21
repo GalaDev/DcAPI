df9f45ab69853152cc8510362765ead314927ba2
Andy Gala <livelifebrands@gmail.com>
2021-04-20 19:54:58 -0700

    Add error handler

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 // Start server
 const app = express();
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
