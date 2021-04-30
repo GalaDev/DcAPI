37ba118391eb4fceaa85a7ede15a5a739bf7a2ea
Andy Gala <livelifebrands@gmail.com>
2021-04-29 20:31:44 -0700

    Add get me controller

--- routes/courses.js
+++ routes/courses.js
@@ -12,6 +12,8 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 router
   .route('/')
   .get(
@@ -21,7 +23,11 @@ router
     }),
     getCourses
   )
-  .post(postCourse);
-router.route('/:id').get(getCourse).put(putCourse).delete(deleteCourse);
+  .post(protect, postCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, putCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
