b78b0e1d31547a7b02160415705cc3043377b141
Andy Gala <livelifebrands@gmail.com>
2021-05-13 20:01:26 -0700

    Add xss package

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const errorHandler = require('./middleware/error');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
-
+const helmet = require('helmet');
+const xss = require('xss-clean');
 // Start server
 const app = express();
 
@@ -42,6 +43,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
